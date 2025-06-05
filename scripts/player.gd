d32dc1b49cda8cda893a1067fa886f3e167a6b8b
panica <unbearable.panic@gmail.com>
2025-06-05 21:17:57 +0400

    Add testing floor and sprites with collision shape for characher and floor

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,12 +5,12 @@ extends CharacterBody2D
 @export var gravity := 1200.0
 
 func _physics_process(delta: float) -> void:
-    var direction := Input.get_axis("ui_left", "ui_right")
-    velocity.x = direction * speed
+	var direction := Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
 
-    if not is_on_floor():
-        velocity.y += gravity * delta
-    elif Input.is_action_just_pressed("ui_up"):
-        velocity.y = jump_velocity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	elif Input.is_action_just_pressed("ui_up"):
+		velocity.y = jump_velocity
 
-    move_and_slide()
+	move_and_slide()
