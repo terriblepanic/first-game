501f6eb9ca3e199d0976585610d3ff73396d1c65
terriblepanic <unbearable.panic@gmail.com>
2025-06-07 12:41:03 +0400

    Add basic HUD and inventory UI

---
+++ scripts/hud.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+@onready var inventory_panel: Control = $HUD/InventoryPanel
+@onready var health_bar: ProgressBar = $HUD/HealthBar
+@onready var mana_bar: ProgressBar = $HUD/ManaBar
+
+func _ready() -> void:
+    var player = get_parent().get_node("Player")
+    if player:
+        if player.has_signal("health_changed"):
+            player.health_changed.connect(_on_player_health_changed)
+        if player.has_signal("mana_changed"):
+            player.mana_changed.connect(_on_player_mana_changed)
+
+func _process(_delta: float) -> void:
+    if Input.is_action_just_pressed("open_inventory"):
+        inventory_panel.visible = not inventory_panel.visible
+
+func _on_player_health_changed(value: int, max_value: int) -> void:
+    health_bar.max_value = max_value
+    health_bar.value = value
+
+func _on_player_mana_changed(value: int, max_value: int) -> void:
+    mana_bar.max_value = max_value
+    mana_bar.value = value
