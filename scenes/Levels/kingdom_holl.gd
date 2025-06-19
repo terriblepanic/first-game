2b2e12d91e92e15a29c00ce7e80143f0ad3ce06b
terriblepanic <unbearable.panic@gmail.com>
2025-06-19 12:42:24 +0400

    Add transition manager and update scene scripts

--- scenes/Levels/kingdom_holl.gd
+++ scenes/Levels/kingdom_holl.gd
@@ -1,17 +1,14 @@
 extends Node2D
 
 func _ready() -> void:
-	var player = $Player/Player
-	var camera = player.get_node("Camera2D")
-	var spawn_point_node = $EntranceRight
-	var spawn_position = spawn_point_node.position
-	var fade_rect = $FX/Fade
-
-	$RightExit.monitoring = false
-	fade_rect.visible = true
-	fade_rect.modulate.a = 1.0
-
-	if spawn_position:
+var player = $Player/Player
+		var camera = player.get_node("Camera2D")
+		var spawn_point_node = $EntranceRight
+		var spawn_position = spawn_point_node.position
+		
+		$RightExit.monitoring = false
+		
+		if spawn_position:
 		# Ставим игрока на позицию заранее
 		player.position = spawn_position + Vector2(100, 0)
 		player.set_process(false)
@@ -21,32 +18,23 @@ func _ready() -> void:
 		
 		# Сбрасываем сглаживание, чтобы камера встала сразу на позицию игрока, без дерганий
 		if camera.position_smoothing_enabled:
-			camera.reset_smoothing()
-
+		camera.reset_smoothing()
+		
 		var tween = get_tree().create_tween()
-
-		tween.tween_property(fade_rect, "modulate:a", 0.0, 1.5)
 		tween.tween_property(player, "position", spawn_position, 1.0)
 		tween.tween_callback(Callable(self, "_enable_transition_and_player"))
-	else:
+		else:
 		player.position = Vector2(100, 100)
-
-func _enable_transition_and_player():
+		
+	func _enable_transition_and_player():
 	var player = $Player/Player
 	player.set_process(true)
 	$RightExit.monitoring = true
-
 	
-func _on_right_exit_body_entered(body: Node2D) -> void:
+	func _on_right_exit_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		var fade_rect = $FX/Fade
-		fade_rect.visible = true
-		fade_rect.modulate.a = 0.0
-		var tween = get_tree().create_tween()
-		tween.tween_property(fade_rect, "modulate:a", 1.0, 0.5)
-		body.set_process(false)
-		tween.tween_property(body, "position:x", body.position.x + 100, 1.0)
-		tween.tween_callback(Callable(self, "_load_next_scene"))
-
-func _load_next_scene():
-	get_tree().change_scene_to_file("res://scenes/Levels/kingdom_king.tscn")
+	body.set_process(false)
+	TransitionManager.change_scene("res://scenes/Levels/kingdom_king.tscn")
+	
+	func _load_next_scene():
+	pass
