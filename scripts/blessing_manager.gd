61b0512f8a0981a1a80c8a7f806d11ad8b020901
terriblepanic <unbearable.panic@gmail.com>
2025-06-19 13:05:14 +0400

    Add pantheon altar interaction

---
+++ scripts/blessing_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name BlessingManager
+
+var blessings: Dictionary = {}
+
+func set_blessing(god_name: String, blessing: String) -> void:
+        blessings[god_name] = blessing
+
+func get_blessing(god_name: String) -> String:
+        return blessings.get(god_name, "")
+
+func clear() -> void:
+        blessings.clear()
